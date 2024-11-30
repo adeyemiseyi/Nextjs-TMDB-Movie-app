67a4ce55a37a6015869e249b753528f0baa8f840
adeyemiseyi <adeyemi.seyi.mukhtar@gmail.com>
2024-11-30 13:04:13 -0800

    File Modification

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavbarItem from './NavebarItem'
 
+
 export default function NavBar() {
   return (
     <div className='flex text-stone-100 dark:bg-gray-600 bg-slate-600 p-4 lg:text-lg justify-center gap-6'>
--- app/components/NavebarItem.jsx
+++ app/components/NavebarItem.jsx
@@ -1,51 +1,5 @@
 "use client"
 
-import React, { Suspense } from 'react';
-import Link from 'next/link';
-import { useSearchParams } from 'next/navigation';
-
-const NavebarItem = ({ title, param }) => {
-    const searchParams = useSearchParams();
-    const genre = searchParams.get('genre');
-
-    return (
-        <div>
-            <Link
-                className={`hover:text-slate-400 
-                        font-semibold ${
-                        genre === param
-                            ? "underline decoration-4 underline-offset-8 decoration-slate-300 rounded-lg"
-                            : ""
-                    }`} 
-                href={`/?genre=${param}`}
-            >
-                {title}
-            </Link>
-        </div>
-    );
-};
-
-// Make sure to wrap your component in Suspense when it's used
-const MyPage = () => (
-  <Suspense fallback={<div>Loading...</div>}>
-    <NavebarItem title="My Title" param="action" />
-  </Suspense>
-);
-
-export default MyPage;
-
-
-
-
-
-
-
-
-
-
-
-/*"use client"
-
 import Link from 'next/link'
 import React from 'react'
 import { useSearchParams } from 'next/navigation'
@@ -69,4 +23,4 @@ export default function NavebarItem({title, param}) {
      </div>
   )
 }
-  */
+
